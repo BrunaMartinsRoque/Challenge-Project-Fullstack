803ce6ba887aa50f69fbfc2535d196ab65b4c1c7
Bruna <buhh_martins@hotmail.com>
2021-12-07 22:58:20 -0300

    arrumando controller

--- src/controller/indexController.js
+++ src/controller/indexController.js
@@ -1,33 +1,5 @@
 const { User } = require("../models");
 
-// const indexController = {
-//   index(req, res) {
-//     res.render("home");
-//   },
-//   async salvar(req, res) {
-//     const { name, email, message } = req.body;
-//     if (name === "" || email === "" || message === "") {
-//       res.send("insira os dados");
-//     // } else if (name === undefined || email === null || message === null) {
-//     //   res.send("verifique os dados");
-//     } else if (
-//       name === undefined ||
-//       email === undefined ||
-//       message === undefined
-//     ) {
-//       res.send("dados invalidos");
-//     } else {
-//       const resultado = await User.create({
-//         name,
-//         email,
-//         message,
-//       });
-
-//       console.log(resultado);
-//       return res.render("home");
-//     }
-//   },
-// };
 const indexController = {
 	index(req,res){
 		res.render('home')
